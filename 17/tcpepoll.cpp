000373fe77043290908ace7148625752143bbaff
PitifulDs <1573014664@qq.com>
2025-08-03 22:30:58 +0800

    Connection对象生命周期 上

---
+++ 17/tcpepoll.cpp
@@ -0,0 +1,35 @@
+/*
+ * 程序名：tcpepoll.cpp，此程序用于演示采用epoll模型实现网络通讯的服务端。
+ * 作者：吴从周
+*/
+// #include <stdio.h>
+// #include <unistd.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include <errno.h>
+// #include <sys/socket.h>
+// #include <sys/types.h>          
+// #include <arpa/inet.h>
+// #include <sys/fcntl.h>
+// #include <sys/epoll.h>
+// #include <netinet/tcp.h>      // TCP_NODELAY需要包含这个头文件。
+// #include "InetAddress.h"
+// #include "Socket.h"
+// #include "Epoll.h"
+// #include "EventLoop.h"
+#include "TcpServer.h"
+int main(int argc,char *argv[])
+{
+    if (argc != 3) 
+    { 
+        printf("usage: ./tcpepoll ip port\n");
+        printf("example: ./tcpepoll 192.168.110.128 5058\n\n");
+        return -1; 
+    }
+    
+    TcpServer tcpserver(argv[1], atoi(argv[2]));
+    
+    tcpserver.start();
+
+  return 0;
+}
